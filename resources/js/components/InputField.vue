01ac7e5dc2c8b282a44342d35695b221751f3da6
braemma <asiamahemma95@gmail.com>
2020-10-06 21:13:01 +0000

    Add Contact View

---
+++ resources/js/components/InputField.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="relative pb-4">
+        <label :for="name" class="pt-2 text-blue-500 uppercase text-xs font-bold absolute">{{label}}</label>
+        <input :id="name" class="pt-8 w-full text-gray-900 border-b pb-2
+                focus:outline-none focus:border-blue-400" :class="errorClassObject()"
+               type="text" :placeholder="placeholder" v-model="value" @input="updateField()">
+
+        <p class="text-red-600 text-sm" v-text="errorMessage()">Errors</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputField",
+
+        props: [
+            'name', 'label', 'placeholder', 'errors'
+        ],
+
+        data: function () {
+            return {
+                value: ''
+            }
+        },
+
+        computed:{
+            hasError: function () {
+                return this.errors && this.errors[this.name]
+                    && this.errors[this.name].length > 0;
+            }
+        },
+
+        methods: {
+            updateField: function () {
+
+                this.clearErrors(this.name);
+                this.$emit('update-field', this.value)
+            },
+            errorMessage: function () {
+                if (this.hasError) {
+                    return this.errors[this.name][0];
+                }
+            },
+            clearErrors: function () {
+                if (this.hasError) {
+                    return this.errors[this.name] = null;
+                }
+            },
+            errorClassObject: function () {
+                return {
+                    'error-field': this.hasError
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .error-field {
+        @apply .border-red-500 .border-b-2
+    }
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\AppController::class, 'index'])->name('home');
+Route::get('/{any}', [AppController::class, 'index'])->where('any', '.*');
+
+Route::get('/logout-manual', function () {
+    request()->session()->invalidate();
+});
